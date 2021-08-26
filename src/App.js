33dd6b3d5e5a3fd565127a1bccc4d2d6f4c58e06
sainathomdas <sainathomdas@gmail.com>
2021-08-26 08:48:10 +0530

    simplify routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Mousey from "./components/Mousey";
+import Projects from "./components/Projects";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
-
 import ViewProject from "./components/ViewProject";
-import Portfolio from "./components/Portfolio";
 
 function App() {
 
@@ -18,8 +22,16 @@ function App() {
       <div className="App">
         <Switch>
         <Route  path="/view-project" exact component={ViewProject} />
-        <Route path="/" exact component={Portfolio} />
-        <Route component={Portfolio}/>
+        <Route path={["/", "/*"]} exact>
+          <>
+        <Header />
+        <Mousey />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </>
+      </Route>
         </Switch>
       </div>
     </BrowserRouter>
